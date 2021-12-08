4524a0cf7bf96784a9082b8225dfe7e9cf070c81
FAISAL.MASUM <FAISAL.MASUM@FAISAL.mynetworksettings.com>
2021-12-07 23:16:24 -0600

    this folder have only java practice file

---
+++ Java_Practice/src/java_Practice/Jb.java
@@ -0,0 +1,136 @@
+package java_Practice;
+
+public class Jb {
+	
+	public void xtest(){
+	
+	String x = "WE ARE GOING TO LA";
+	String y = "WE ARE GOINT TO NEWWORK";
+	
+ int a = 6;
+ int b = 7;
+ int c = a*b/b;
+ 
+System.out.println(c);
+System.out.println(x);
+System.out.println(y);
+
+if(x ==y) {
+	System.out.println("TRUE");
+}
+else {
+	System.out.println("FALSE");
+}
+System.out.println("________________________________________________________");
+	}
+	
+	public void dtest(){
+	
+
+	String f = "WE ARE GOING TO LAsxd";
+	String k = "WE ARE GOINT TO NEWWdxssORK";
+	
+	int r = 6;
+	int t = 7;
+	int e = r*t/r;
+ 
+System.out.println(e);
+System.out.println(f);
+System.out.println(k);
+
+if(f !=k) {
+	System.out.println("TRUE");
+}
+else {
+	System.out.println("FALSE");
+}
+System.out.println("________________________________________________________");
+}
+	public void gtest(String exr, String axr) {
+		
+		exr= "we are runing a test for db";
+		axr = "we are runing a test for dbg";
+		
+	if(exr ==axr) {
+			System.out.println("PASS");
+	}		
+	else
+			{
+			System.out.println("FAIL");
+		}
+	System.out.println("________________________________________________________");
+		}
+	
+	
+	public void Vtest() {
+		
+		String l = "YOU ARE NOT ONE OF MY BEST FRESNDS";
+		
+		int w = l.length();
+		int m = l.indexOf("YOU");
+		int p= l.indexOf("ARE NOT ONE OF MY BEST FRESNDS");
+		System.out.println(w);
+		System.out.println(m);
+		System.out.println(p);
+		
+		System.out.println("________________________________________________________");
+		
+	}
+	public void Qtest(){
+		
+	int u =6;
+	int f =10;
+	int j = u+f;
+	
+	for(int i=10; i<j; i++) {
+		System.out.println("YOU ARE RIGHT");
+	}
+	System.out.println("________________________________________________________");
+	
+	}
+	public void Pytest() {
+		
+		
+		int[] py = {34,78,66,56,67,55};
+		
+		
+			for(int i=0; i<py.length; i++) {
+			System.out.println(py[i]);
+			//System.out.println("________________________________________________________");		
+			//System.out.println(py[1]);
+			
+			
+			
+		}
+		System.out.println("________________________________________________________");	
+		
+		
+		
+		
+	}
+	
+	public void Listtest() {
+		
+				
+	String[] list = new String[4];
+	list[0]="jon";
+	list[1]="mike";
+	list[2]="jame";
+	
+	System.out.println(list[1]);
+	
+	
+	
+	
+	
+		
+	}
+	
+
+	
+	}
+
+
+	
+
+
---
+++ Java_Practice/src/java_Practice_Test/Jb_Test.java
@@ -0,0 +1,21 @@
+package java_Practice_Test;
+
+import java_Practice.Jb;
+
+public class Jb_Test {
+
+	public static void main(String[] args) {
+	
+		// this is call the instantiate
+		Jb j = new Jb();
+		
+		j.xtest();
+		j.dtest();
+		j.gtest("","");
+		j.Vtest();
+		j.Qtest();
+		j.Pytest();
+		j.Listtest();
+	}
+
+}
