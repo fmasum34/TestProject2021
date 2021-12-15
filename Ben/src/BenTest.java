8c9f29f0db067a233cfe1869f1ec5dea8febc42c
FAISAL.MASUM <FAISAL.MASUM@FAISAL.mynetworksettings.com>
2021-12-14 22:40:57 -0600

    this is Ben java test

---
+++ Ben/src/BenTest.java
@@ -0,0 +1,27 @@
+
+public class BenTest {
+
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			String x = "WE ARE GOING TO LA";
+			String y = "WE ARE GOINT TO NEWWORK";
+			
+		 
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		//System.out.println(x+y);
+		//System.out.println(y+x);
+
+		if(x ==y) {
+			System.out.println("TRUE");
+		}
+		else {
+			System.out.println("FALSE");
+		}
+		System.out.println("________________________________________________________");
+			}
+	}
+
