a30e6fb6f49be05d876cb64ea7608fcb6c965a3b
faisal <fmasum34@gmail.com>
2024-07-21 16:38:20 -0400

    THIS IS THE TEST FROM ECLIPS AND GITHUB

---
+++ GITHUBTESTPROJECT/src/gitHubTest/GitHubTest.java
@@ -0,0 +1,9 @@
+package gitHubTest;
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		System.out.println(" THIS IS A TEST TO MAKRE SURE GITHUB CONNECT IS WORKING FINE");
+	}
+
+}
